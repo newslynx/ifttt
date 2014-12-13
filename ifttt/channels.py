4dded30c0ae50bbd9c8e6edc43ff21070ce539e4
abelsonlive <brianabelson@gmail.com>
2014-12-13 18:49:44 -0500

    updates

--- ifttt/channels.py
+++ ifttt/channels.py
@@ -1,24 +1,20 @@
-channels = \
-{
+channels = {
     "twitter": {
         "user_name": "{{UserName}}",
         "published": "{{CreatedAt}}",
         "short_url": "{{LinkToTweet}}",
         "text": "{{Text}}"
     },
-{
     "yo": {
         "published": "{{ReceivedAt}}",
         "from": "{{From}}"
     },
-{
     "craigslist": {
         "text": "{{PostContent}}",
         "title": "{{PostTitle}}",
         "url": "{{PostUrl}}",
         "published": "{{PostPublished}}"
     },
-{
     "instagram": {
         "caption": "{{Caption}}",
         "url": "{{Url}}",
