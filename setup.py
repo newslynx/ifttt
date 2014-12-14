f008eb1e4473fcc05f4695749b7876b5090ea458
abelsonlive <brianabelson@gmail.com>
2014-12-14 00:38:32 -0500

    updates

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme(fname):
 def build():
     setup(
         name = "ifttt",
-        version = "0.0.1",
+        version = "0.2.0",
         author = "Brian Abelson",
         author_email = "brian@newslynx.org",
         description = "A Pythonic interface for building IFTTT plugins routed over email.",
