ecba57dadc65b348d441ce2b9d2b4e5426fea2d9
abelsonlive <brianabelson@gmail.com>
2014-12-14 02:31:56 -0500

    hmmm

--- setup.py
+++ setup.py
@@ -7,21 +7,21 @@ def readme(fname):
 def build():
     setup(
         name = "ifttt",
-        version = "0.2.8",
+        version = "0.2.9",
         author = "Brian Abelson",
         author_email = "brian@newslynx.org",
         description = "A Pythonic interface for building IFTTT plugins routed over email.",
         license = "MIT",
         keywords = "email, ifttt",
         url = "https://github.com/newslynx/ifttt",
-        packages=['ifttt'],
-        long_description=readme('README.md'),
+        packages = ['ifttt'],
+        long_description = readme('README.md'),
         install_requires = ["pytz", "gevent"],
         classifiers=[
             "Development Status :: 3 - Alpha",
             "Topic :: Communications :: Email",
             "License :: OSI Approved :: MIT License",
-        ],
+        ]
     )
 
 if __name__ == '__main__':
